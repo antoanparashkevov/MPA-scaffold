88aabf88f9d3fef804bd5aa4e66d43ac03fc469a
“antoanparashkevov” <tonkata1505@gmail.com>
2022-10-29 13:45:14 +0300

    Try Catch for handling the errors during the registration process.

--- contollers/authController.js
+++ contollers/authController.js
@@ -9,26 +9,39 @@ router.get('/register', (req,res) => {
 })
 
 router.post('/register', async (req,res) => {
-    const formData = req.body;
-    //TODO remove the log
-    console.log('formData from the register form >>> ', formData)
+        const formData = req.body;
+        //TODO remove the log
+        console.log('formData from the register form >>> ', formData)
     
-    //TODO change the error handling to correspond to the assignment
-    if(formData.password.trim() !== formData.repass.trim()) {
-        throw new Error("The password doesn't match!")
-    }
+        try {
+            //TODO change the error handling to correspond to the assignment
+            if (formData.password !== formData.repass) {
+                throw new Error("The password doesn't match!")
+            }
     
-    if(!formData.username || !formData.password || !formData.repass || !formData.email) {
-        throw new Error("All fields are required!")
-    }
+            if (!formData.username || !formData.password || !formData.repass || !formData.email) {
+                throw new Error("All fields are required!")
+            }
     
-    //the json web token
-    const token = await register(formData.username, formData.email, formData.password);
+            //the json web token
+            const token = await register(formData.username, formData.email, formData.password);
     
-    //set as a cookie our JSON Web Token
-    res.cookie('token', token);
+            //set as a cookie our JSON Web Token
+            res.cookie('token', token);
     
-    res.redirect('/auth/register')//if we don't redirect, it will load the page forever
+            res.redirect('/auth/register')//if we don't redirect, it will load the page forever}
+        } catch (error) {
+            //TODO parse the error obj
+            const errors = [error.message]
+            //TODO add error display to the actual template from the assignment
+            res.render('pages/register', {
+                title: 'Register page',
+                errors,
+                body: {
+                    username: formData.username
+                }
+            })
+        }
 })
 
 module.exports = router;
