a0c356f1598eae4b6878ce4815d71a5d3008dc16
“antoanparashkevov” <tonkata1505@gmail.com>
2022-10-29 12:34:45 +0300

    Database configuration

--- config/database.js
+++ config/database.js
@@ -1,3 +1,17 @@
-module.exports = () => {
-    
+const mongoose = require('mongoose');
+
+//TODO replace with the database name from the assignment
+const CONNECTION_STRING = 'mongodb://localhost:27017/scaffold'
+
+module.exports = async () => {
+    try {
+        await mongoose.connect(CONNECTION_STRING, {
+            useUnifiedTopology: true,
+            useNewUrlParser: true,
+        })
+        console.log('Database connected')
+    } catch (err) {
+        console.error(err.message);
+        process.exit(1)
+    }
 }
